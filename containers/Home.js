36bdad8b7aa4ab62d7b812c019639c73a55a436e
karthiksista <karthik.sista6@gmail.com>
2019-07-04 14:08:24 +0530

    1.

--- containers/Home.js
+++ containers/Home.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 class Home extends React.Component {
   render() {
-    return <h1> Thdfgbdfgdfgis is the Home page </h1>;
+    return (
+      <h1> Thdfgbdfgdfgis sdvsdvsdvdsvsdvsdvdsvsdvvsdvsdvis the Home page </h1>
+    );
   }
 }
 
